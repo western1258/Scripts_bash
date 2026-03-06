6d4ed2f5e3b1c0db6f4845ab0b1674f6ba8e3272
Juan Elias Antonio Ramirez <122416882+western1258@users.noreply.github.com>
2026-03-06 08:36:14 -0600

    Modify set_ip.sh for IP prefix configuration
    Updated IP configuration script to use prefix instead of subnet mask.

--- set_ip.sh
+++ set_ip.sh
@@ -1,14 +1,13 @@
 #! /bin/bash
-# el archivo de configuracion de ip debe venir con el siguiente orden interfaz, direccion ip,mascara, gateway
+# el archivo de configuracion de ip debe venir con el siguiente orden interfaz, direccion ip/prefijo, gateway
 
 while read -r linea;
 do
 	iface=$(echo "$linea" | cut -d ',' -f 1)
 	ip=$(echo "$linea" | cut -d ',' -f 2)
-	masc=$(echo "$linea" | cut -d ',' -f 3)
-	gate=$(echo "$linea" | cut -d ',' -f 4)
+	gate=$(echo "$linea" | cut -d ',' -f 3)
 
-	ip addr add "$ip" "$masc" dev "$iface"
+	ip addr add "$ip" dev "$iface"
 	ip link set "$iface" up
 	ip route add default via "$gate" dev "$iface"
 
